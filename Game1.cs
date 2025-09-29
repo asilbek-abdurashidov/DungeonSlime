3fde4beb14be7ea701cb32da9cbf5a41d1a9b0e6
aabdurashidov <asilbekdev@gmail.com>
2025-09-29 22:24:59 +0500

    Add code library

--- Game1.cs
+++ Game1.cs
@@ -1,49 +1,47 @@
-﻿using System;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGameLibrary;
 
 namespace DungeonSlime;
 
-public class Game1 : Game
+public class Game1 : Core
 {
-    private int updateCounter = 0;
-    private int drawCounter = 0;
-    private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
-
-    public Game1()
+    public Game1() : base("Dungeon Slime", 1280, 720, false)
     {
-        _graphics = new GraphicsDeviceManager(this);
-        Content.RootDirectory = "Content";
-        IsMouseVisible = true;
+
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
     }
 
     protected override void LoadContent()
     {
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        // TODO: use this.Content to load your game content here
+
+        base.LoadContent();
     }
 
     protected override void Update(GameTime gameTime)
     {
-        updateCounter++;
-        Console.WriteLine("Updating " + updateCounter);
-        var isBackPressed = GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed;
-        var isEscapePressed = Keyboard.GetState().IsKeyDown(Keys.Escape);
-
-        if (isBackPressed || isEscapePressed)
-        {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
-        }
+
+        // TODO: Add your update logic here
 
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
-        drawCounter++;
-        Console.WriteLine("Drawing " + drawCounter);
         GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        // TODO: Add your drawing code here
+
         base.Draw(gameTime);
     }
 }
